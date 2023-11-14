045360d00297d3dbd62de392a3d67a7673562177
TharinduGee <tharindugeethanjan@gmail.com>
2023-11-14 19:42:14 +0530

    initial commit

---
+++ lib/screen/login.dart
@@ -0,0 +1,236 @@
+import 'package:flutter/material.dart';
+import 'package:namer_app/screen/Signup.dart';
+
+class LogIn extends StatefulWidget {
+  @override
+  State<LogIn> createState() => _LogInState();
+}
+
+final _formKey = GlobalKey<FormState>();
+//Allowed characters for email
+String allowed =
+    r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$';
+RegExp regExp = RegExp(allowed);
+void validation() {
+  final form = _formKey.currentState;
+  if (form!.validate()) {
+    print("Yes");
+  } else {
+    print("No");
+  }
+}
+
+class _LogInState extends State<LogIn> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      //backgroundColor: ColorScheme.fromSeed(seedColor: Colors.deepPurpleAccent),
+      body: Form(
+      key: _formKey,
+      child: SingleChildScrollView(
+        physics: BouncingScrollPhysics(),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Padding(
+                padding: const EdgeInsets.only(
+                  left: 15,
+                  top: 5,
+                ),
+                child: Container(
+                  alignment: Alignment.center,
+                  height: 300,
+                  child: Image(
+                    image: AssetImage("assets/images/vector-1.png"),
+                  ),
+                )),
+            const SizedBox(
+              height: 18,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 50),
+              child: Column(
+                textDirection: TextDirection.ltr,
+                children: [
+                  const Text(
+                    'Log In',
+                    style: TextStyle(
+                      color: Color(0xFF755DC1),
+                      fontSize: 27,
+                      fontFamily: 'Poppins',
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 50,
+                  ),
+                  TextFormField(
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return "Please Enter Email";
+                      } else if (!regExp.hasMatch(value)) {
+                        return "Email in invalid";
+                      }
+                      return null;
+                    },
+                    textAlign: TextAlign.center,
+                    style: const TextStyle(
+                      color: Color(0xFF393939),
+                      fontSize: 13,
+                      fontFamily: 'Poppins',
+                      fontWeight: FontWeight.w400,
+                    ),
+                    decoration: const InputDecoration(
+                      labelText: 'Email',
+                      labelStyle: TextStyle(
+                        color: Color(0xFF755DC1),
+                        fontSize: 15,
+                        fontFamily: 'Poppins',
+                        fontWeight: FontWeight.w600,
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(10)),
+                        borderSide: BorderSide(
+                          width: 1,
+                          color: Color(0xFF837E93),
+                        ),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(10)),
+                        borderSide: BorderSide(
+                          width: 1,
+                          color: Color(0xFF9F7BFF),
+                        ),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 50,
+                  ),
+                  TextFormField(
+                    obscureText: true,
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return "Please fill Password";
+                      } else if (value.length < 8) {
+                        return "Password is too short";
+                      }
+                      return null;
+                    },
+                    textAlign: TextAlign.center,
+                    style: const TextStyle(
+                      color: Color(0xFF393939),
+                      fontSize: 13,
+                      fontFamily: 'Poppins',
+                      fontWeight: FontWeight.w400,
+                    ),
+                    decoration: const InputDecoration(
+                      labelText: 'Password',
+                      labelStyle: TextStyle(
+                        color: Color(0xFF755DC1),
+                        fontSize: 15,
+                        fontFamily: 'Poppins',
+                        fontWeight: FontWeight.w600,
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(10)),
+                        borderSide: BorderSide(
+                          width: 1,
+                          color: Color(0xFF837E93),
+                        ),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(10)),
+                        borderSide: BorderSide(
+                          width: 1,
+                          color: Color(0xFF9F7BFF),
+                        ),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 25,
+                  ),
+                  ClipRRect(
+                    borderRadius: const BorderRadius.all(Radius.circular(10)),
+                    child: SizedBox(
+                      width: 329,
+                      height: 56,
+                      child: ElevatedButton(
+                        onPressed: () {
+                          validation();
+                        },
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: const Color(0xFF9F7BFF),
+                        ),
+                        child: const Text(
+                          'Sign In',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 15,
+                            fontFamily: 'Poppins',
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 15,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const Text(
+                        'Don’t have an account?',
+                        style: TextStyle(
+                          color: Color(0xFF837E93),
+                          fontSize: 13,
+                          fontFamily: 'Poppins',
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                      const SizedBox(
+                        width: 2.5,
+                      ),
+                      InkWell(
+                        onTap: () {
+                          // widget.controller.animateToPage(1,
+                          // duration: const Duration(milliseconds: 500),
+                          // curve: Curves.ease);
+                          Navigator.of(context).pushReplacement(
+                              MaterialPageRoute(builder: (ctx) => Signup()));
+                        },
+                        child: Text(
+                          'Sign Up',
+                          style: TextStyle(
+                            color: Color(0xFF755DC1),
+                            fontSize: 13,
+                            fontFamily: 'Poppins',
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 15,
+                  ),
+                  const Text(
+                    'Forget Password?',
+                    style: TextStyle(
+                      color: Color(0xFF755DC1),
+                      fontSize: 13,
+                      fontFamily: 'Poppins',
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    ));
+  }
+}
---
+++ lib/screen/signup.dart
@@ -0,0 +1,363 @@
+import 'package:flutter/material.dart';
+import 'package:namer_app/screen/login.dart';
+
+class Signup extends StatefulWidget {
+  @override
+  State<Signup> createState() => _SignupState();
+}
+
+final _formKey = GlobalKey<FormState>();
+String allowed =
+    r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$';
+RegExp regExp = RegExp(allowed);
+void validation() {
+  final form = _formKey.currentState;
+  if (form!.validate()) {
+    print("Yes");
+  } else {
+    print("No");
+  }
+}
+
+const o = OutlineInputBorder(
+  borderRadius: BorderRadius.all(Radius.circular(10)),
+  borderSide: BorderSide(
+    width: 1,
+    color: Color(0xFF837E93),
+  ),
+);
+
+class _SignupState extends State<Signup> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Form(
+          key: _formKey,
+          child: SingleChildScrollView(
+            physics: BouncingScrollPhysics(),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.only(
+                    top: 5,
+                  ),
+                  child: Image(
+                    image: AssetImage("assets/images/vector-2.png"),
+         
+                    height: 250,
+                  ),
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 50),
+                  child: Column(
+                    textDirection: TextDirection.ltr,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      const Text(
+                        'Register',
+                        style: TextStyle(
+                          color: Color(0xFF755DC1),
+                          fontSize: 27,
+                          fontFamily: 'Poppins',
+                          fontWeight: FontWeight.w700,
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      TextFormField(
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return "Username is Too Short";
+                          } else if (value.length < 6) {
+                            return "Please Enter a Username";
+                          }
+                          return null;
+                        },
+                        textAlign: TextAlign.center,
+                        style: const TextStyle(
+                          color: Color(0xFF393939),
+                          fontSize: 13,
+                          fontFamily: 'Poppins',
+                          fontWeight: FontWeight.w400,
+                        ),
+                        decoration: const InputDecoration(
+                          labelText: 'UserName',
+                          labelStyle: TextStyle(
+                            color: Color(0xFF755DC1),
+                            fontSize: 15,
+                            fontFamily: 'Poppins',
+                            fontWeight: FontWeight.w600,
+                          ),
+                          enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.all(Radius.circular(10)),
+                            borderSide: BorderSide(
+                              width: 1,
+                              color: Color(0xFF837E93),
+                            ),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.all(Radius.circular(10)),
+                            borderSide: BorderSide(
+                              width: 1,
+                              color: Color(0xFF9F7BFF),
+                            ),
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 17,
+                      ),
+                      TextFormField(
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return "Please Enter Email";
+                          } else if (!regExp.hasMatch(value)) {
+                            return "Your Email is Invalid";
+                          }
+                          return null;
+                        },
+                        textAlign: TextAlign.center,
+                        style: const TextStyle(
+                          color: Color(0xFF393939),
+                          fontSize: 13,
+                          fontFamily: 'Poppins',
+                          fontWeight: FontWeight.w400,
+                        ),
+                        decoration: const InputDecoration(
+                          labelText: 'Email',
+                          labelStyle: TextStyle(
+                            color: Color(0xFF755DC1),
+                            fontSize: 15,
+                            fontFamily: 'Poppins',
+                            fontWeight: FontWeight.w600,
+                          ),
+                          enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.all(Radius.circular(10)),
+                            borderSide: BorderSide(
+                              width: 1,
+                              color: Color(0xFF837E93),
+                            ),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.all(Radius.circular(10)),
+                            borderSide: BorderSide(
+                              width: 1,
+                              color: Color(0xFF9F7BFF),
+                            ),
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 17,
+                      ),
+                      TextFormField(
+                        validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return "Please fill Password";
+                      } else if (value.length < 8) {
+                        return "Password is too short";
+                      }
+                      return null;
+                    },
+                        obscureText: true,
+                        textAlign: TextAlign.center,
+                        style: const TextStyle(
+                          color: Color(0xFF393939),
+                          fontSize: 13,
+                          fontFamily: 'Poppins',
+                          fontWeight: FontWeight.w400,
+                        ),
+                        decoration: const InputDecoration(
+                          labelText: 'Password',
+                          labelStyle: TextStyle(
+                            color: Color(0xFF755DC1),
+                            fontSize: 15,
+                            fontFamily: 'Poppins',
+                            fontWeight: FontWeight.w600,
+                          ),
+                          enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.all(Radius.circular(10)),
+                            borderSide: BorderSide(
+                              width: 1,
+                              color: Color(0xFF837E93),
+                            ),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.all(Radius.circular(10)),
+                            borderSide: BorderSide(
+                              width: 1,
+                              color: Color(0xFF9F7BFF),
+                            ),
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 17,
+                      ),
+                      TextFormField(
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return "Please fill Password";
+                          } else if (value.length < 8) {
+                            return "Password is too short";
+                          }
+                          return null;
+                        },
+                        obscureText: true,
+                        textAlign: TextAlign.center,
+                        style: const TextStyle(
+                          color: Color(0xFF393939),
+                          fontSize: 13,
+                          fontFamily: 'Poppins',
+                          fontWeight: FontWeight.w400,
+                        ),
+                        decoration: const InputDecoration(
+                          labelText: 'Confirm password',
+                          labelStyle: TextStyle(
+                            color: Color(0xFF755DC1),
+                            fontSize: 15,
+                            fontFamily: 'Poppins',
+                            fontWeight: FontWeight.w600,
+                          ),
+                          enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.all(Radius.circular(10)),
+                            borderSide: BorderSide(
+                              width: 1,
+                              color: Color(0xFF837E93),
+                            ),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.all(Radius.circular(10)),
+                            borderSide: BorderSide(
+                              width: 1,
+                              color: Color(0xFF9F7BFF),
+                            ),
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 17,
+                      ),
+                      TextFormField(
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return "Please fill Phone No";
+                          } else if (value.length < 8) {
+                            return "Invalid Phone No";
+                          }
+                          return null;
+                        },
+                        textAlign: TextAlign.center,
+                        style: const TextStyle(
+                          color: Color(0xFF393939),
+                          fontSize: 13,
+                          fontFamily: 'Poppins',
+                          fontWeight: FontWeight.w400,
+                        ),
+                        decoration: const InputDecoration(
+                          labelText: 'Phone No',
+                          labelStyle: TextStyle(
+                            color: Color(0xFF755DC1),
+                            fontSize: 15,
+                            fontFamily: 'Poppins',
+                            fontWeight: FontWeight.w600,
+                          ),
+                          enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.all(Radius.circular(10)),
+                            borderSide: BorderSide(
+                              width: 1,
+                              color: Color(0xFF837E93),
+                            ),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.all(Radius.circular(10)),
+                            borderSide: BorderSide(
+                              width: 1,
+                              color: Color(0xFF9F7BFF),
+                            ),
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 25,
+                      ),
+                      ClipRRect(
+                        borderRadius: const BorderRadius.all(Radius.circular(10)),
+                        child: SizedBox(
+                          width: 329,
+                          height: 56,
+                          child: ElevatedButton(
+                            onPressed: () {
+                              // widget.controller.animateToPage(2,
+                              //     duration: const Duration(milliseconds: 500),
+                              //     curve: Curves.ease);
+                              validation();
+                            },
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor: const Color(0xFF9F7BFF),
+                            ),
+                            child: const Text(
+                              'Create account',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 15,
+                                fontFamily: 'Poppins',
+                                fontWeight: FontWeight.w500,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 15,
+                      ),
+                      Row(
+                        children: [
+                          const Text(
+                            ' Have an account?',
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                              color: Color(0xFF837E93),
+                              fontSize: 13,
+                              fontFamily: 'Poppins',
+                              fontWeight: FontWeight.w500,
+                            ),
+                          ),
+                          const SizedBox(
+                            width: 3.5,
+                          ),
+                          InkWell(
+                            onTap: () {
+                              // widget.controller.animateToPage(0,
+                              //     duration: const Duration(milliseconds: 500),
+                              //     curve: Curves.ease);
+                              Navigator.of(context).pushReplacement(
+                                MaterialPageRoute(builder: (ctx) => LogIn()),
+                              );
+                            },
+                            child: const Text(
+                              'Log In ',
+                              style: TextStyle(
+                                color: Color(0xFF755DC1),
+                                fontSize: 13,
+                                fontFamily: 'Poppins',
+                                fontWeight: FontWeight.w500,
+                              ),
+                            ),
+                          ),
+                        ],
+                      )
+                    ],
+                  ),
+                )
+              ],
+            ),
+          )),
+    );
+  }
+}
