4234b28ebc18f78766b4c2c428ac6a8da111cfe7
TharinduGee <tharindugeethanjan@gmail.com>
2023-11-25 14:51:37 +0530

    development change

--- lib/screen/home.dart
+++ lib/screen/home.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:namer_app/screen/gallery.dart';
 
 class Home extends StatelessWidget {
   Home({super.key});
 
-  final user = FirebaseAuth.instance.currentUser;
+final user = FirebaseAuth.instance.currentUser;
   void logOut() {
     FirebaseAuth.instance.signOut();
   }
@@ -12,17 +13,25 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      drawer :  Drawer(),
-      appBar: AppBar(
-        title: Icon(Icons.home_filled),
-        centerTitle:  true,
-        actions: [
-        IconButton(onPressed: logOut, icon: Icon(Icons.logout)), 
-         //IconButton(onPressed: logOut, icon: Icon(Icons.verified_user_sharp)),
-        
+      drawer: Drawer(),
+      appBar:
+          AppBar(title: Icon(Icons.home_filled), centerTitle: true, actions: [
+        IconButton(onPressed: logOut, icon: Icon(Icons.logout)),
+      
+        //IconButton(onPressed: logOut, icon: Icon(Icons.verified_user_sharp)),
       ]),
       body: Center(
-        child: Text("Logged In"),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text("Logged In"),
+            IconButton(onPressed: (){Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => const Gallery()),
+            );},  icon: Icon(Icons.photo_camera_back_outlined) , iconSize: 200),
+          ],
+        ),
+        
       ),
     );
   }
